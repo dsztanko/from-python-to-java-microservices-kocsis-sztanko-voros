561c2a90a17c1202f892b290a214ce280c0d9003
vbenedek <xbenedek01@gmail.com>
2017-01-03 12:23:56 +0100

    saving and getting out data from memory using DAO

--- src/main/java/you_might_also_like_service/service/YouMightAlsoLikeAPIService.java
+++ src/main/java/you_might_also_like_service/service/YouMightAlsoLikeAPIService.java
@@ -1,4 +1,29 @@
 package you_might_also_like_service.service;
 
+import you_might_also_like_service.dao.UserDao;
+import you_might_also_like_service.dao.inmemoryimplementation.UserDaoMem;
+import you_might_also_like_service.model.User;
+
+import java.util.ArrayList;
+
 public class YouMightAlsoLikeAPIService {
+    private static YouMightAlsoLikeAPIService ourInstance = new YouMightAlsoLikeAPIService();
+
+    public static YouMightAlsoLikeAPIService getInstance() {
+        return ourInstance;
+    }
+
+    private UserDao userDao;
+
+    private YouMightAlsoLikeAPIService() {
+        userDao = UserDaoMem.getInstance();
+    }
+
+    public void saveUser(User user) {
+        userDao.save(user);
+    }
+
+    public ArrayList getAllByItems(ArrayList items) {
+        return userDao.selectByCartItems(items);
+    }
 }
