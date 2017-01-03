d1749136faf9403c64be4b3694665358e34e6ac5
vbenedek <xbenedek01@gmail.com>
2017-01-03 14:50:06 +0100

    Arguments changed

--- src/main/java/you_might_also_like_service/dao/UserDao.java
+++ src/main/java/you_might_also_like_service/dao/UserDao.java
@@ -5,6 +5,6 @@ import you_might_also_like_service.model.User;
 import java.util.ArrayList;
 
 public interface UserDao {
-    public void save(User user);
+    public void save(String accessToken, String userID, String item);
     public ArrayList selectByCartItems(ArrayList<String> items);
 }
