f9dc9be1582ddfb802c66e38bf4211eaa313b2f3
dsztanko <szt.dori@gmail.com>
2017-01-03 18:03:31 +0100

    change to Arraylist<String>

--- src/main/java/you_might_also_like_service/dao/UserDao.java
+++ src/main/java/you_might_also_like_service/dao/UserDao.java
@@ -3,9 +3,11 @@ package you_might_also_like_service.dao;
 import you_might_also_like_service.model.User;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public interface UserDao {
     void save(String accessToken, String userID, String item);
     ArrayList selectByCartItems(String accessToken, String userId);
     User find(String accessToken, String userId);
+    HashMap<String, Integer> selectUniqueItems(String accessToken);
 }
