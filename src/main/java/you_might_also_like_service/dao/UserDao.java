86228b312a82e4006d300d0925be64bb86b1ee20
vbenedek <xbenedek01@gmail.com>
2017-01-03 12:01:52 +0100

    DAO pattern interface with neccessary memory handling methods

--- src/main/java/you_might_also_like_service/dao/UserDao.java
+++ src/main/java/you_might_also_like_service/dao/UserDao.java
@@ -1,4 +1,10 @@
 package you_might_also_like_service.dao;
 
+import you_might_also_like_service.model.User;
+
+import java.util.ArrayList;
+
 public interface UserDao {
+    public void save(User user);
+    public ArrayList selectByCartItems(ArrayList<String> items);
 }
--- src/main/java/you_might_also_like_service/dao/inmemoryimplementation/UserDaoMem.java
+++ src/main/java/you_might_also_like_service/dao/inmemoryimplementation/UserDaoMem.java
@@ -1,4 +1,12 @@
 package you_might_also_like_service.dao.inmemoryimplementation;
 
-public class UserDaoMem {
+public class UserDaoMem2 {
+    private static UserDaoMem2 ourInstance = new UserDaoMem2();
+
+    public static UserDaoMem2 getInstance() {
+        return ourInstance;
+    }
+
+    private UserDaoMem2() {
+    }
 }
