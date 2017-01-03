f265c7213562c32cc69e3db4c7bb26e30fed57d4
dsztanko <szt.dori@gmail.com>
2017-01-03 18:05:36 +0100

    getRecommendations() method in progress

--- src/main/java/you_might_also_like_service/service/YouMightAlsoLikeAPIService.java
+++ src/main/java/you_might_also_like_service/service/YouMightAlsoLikeAPIService.java
@@ -6,9 +6,11 @@ import you_might_also_like_service.dao.inmemoryimplementation.UserDaoMem;
 import you_might_also_like_service.model.User;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class YouMightAlsoLikeAPIService {
     private static YouMightAlsoLikeAPIService ourInstance = new YouMightAlsoLikeAPIService();
+    private static final int BASE_INDEX = 3;
 
     public static YouMightAlsoLikeAPIService getInstance() {
         return ourInstance;
@@ -25,8 +27,21 @@ public class YouMightAlsoLikeAPIService {
     }
 
     public JSONObject getRecommendations(String accessToken, String userId) {
-        ArrayList allusers = userDao.selectByCartItems(accessToken, userId);
-        User user = userDao.find(accessToken, userId);
+//        todo: in progress
+        ArrayList<User> allUsers = userDao.selectByCartItems(accessToken, userId);
+        User specUser = userDao.find(accessToken, userId);
+        HashMap<String, Integer> uniqueItems = userDao.selectUniqueItems(specUser.getAccessToken());
+        for (User user : allUsers) {
+            Integer power = -1;
+            for (String item : user.getItems()) {
+                if (specUser.getItems().contains(item)) {
+                    power++;
+                }
+            }
+            for (String item : user.getItems()) {
+                uniqueItems.put(item, (int) (uniqueItems.get(item) + Math.pow(BASE_INDEX, power)));
+            }
+        }
         return null;
     }
 }
