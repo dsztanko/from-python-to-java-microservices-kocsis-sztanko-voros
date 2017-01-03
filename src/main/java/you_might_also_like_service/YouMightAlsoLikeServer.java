11ffdc713c86835a904f04a44694e6dd9393f29c
dsztanko <szt.dori@gmail.com>
2017-01-03 14:00:37 +0100

    microservice's server implemented

--- src/main/java/you_might_also_like_service/YouMightAlsoLikeServer.java
+++ src/main/java/you_might_also_like_service/YouMightAlsoLikeServer.java
@@ -1,21 +1,34 @@
 package you_might_also_like_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import you_might_also_like_service.controller.YouMightAlsoLikeAPIController;
+import you_might_also_like_service.service.YouMightAlsoLikeAPIService;
 
 import java.net.URISyntaxException;
 
-import static spark.Spark.*;
+import static spark.Spark.exception;
+import static spark.Spark.port;
+import static spark.Spark.get;
 
 public class YouMightAlsoLikeServer {
-//    private YouMightAlsoLikeAPIController controller;
+    private static final Logger logger = LoggerFactory.getLogger(YouMightAlsoLikeServer.class);
+
+    private YouMightAlsoLikeAPIController controller;
 
     public static void main(String[] args) {
+        logger.debug(">>>>> Server started to run: {}", YouMightAlsoLikeServer.class.getName());
+
+        YouMightAlsoLikeServer application = new YouMightAlsoLikeServer();
+
+        application.controller = new YouMightAlsoLikeAPIController(YouMightAlsoLikeAPIService.getInstance());
 
         port(60002);
 
         // --- MAPPINGS ---
-//        get("/api/preferences/save", application.controller::getAllEmail);
-//        get("/api/preferences/select", application.controller::changeStatus);
+        get("api/preferences/status", application.controller::status);
+        get("/api/preferences/save", application.controller::saveToDao);
+        get("/api/preferences/select", application.controller::selectByCartItemsFromDao);
 
         // --- EXCEPTION HANDLING ---
         exception(URISyntaxException.class, (exception, request, response) -> {
