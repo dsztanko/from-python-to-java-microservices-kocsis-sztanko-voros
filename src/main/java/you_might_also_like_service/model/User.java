9b9d38bbc547ab28b8c6602ba9a2e2181cd50426
vbenedek <xbenedek01@gmail.com>
2017-01-03 12:04:33 +0100

    parameters and constructor

--- src/main/java/you_might_also_like_service/model/User.java
+++ src/main/java/you_might_also_like_service/model/User.java
@@ -1,4 +1,18 @@
 package you_might_also_like_service.model;
 
+import java.util.ArrayList;
+
 public class User {
+    private String accessToken, userID;
+    private ArrayList<String> items;
+
+    public User(String accessToken, String userID, ArrayList<String> items) {
+        this.accessToken = accessToken;
+        this.userID = userID;
+        this.items = items;
+    }
+
+    public ArrayList getItems() {
+        return items;
+    }
 }
