aa6af9d8b0b1043364c1065dfb9362fac0efe407
dsztanko <szt.dori@gmail.com>
2017-01-03 14:02:37 +0100

    basic service controller implemented

--- src/main/java/you_might_also_like_service/controller/YouMightAlsoLikeAPIController.java
+++ src/main/java/you_might_also_like_service/controller/YouMightAlsoLikeAPIController.java
@@ -1,4 +1,29 @@
 package you_might_also_like_service.controller;
 
+import spark.Request;
+import spark.Response;
+import you_might_also_like_service.service.YouMightAlsoLikeAPIService;
+
 public class YouMightAlsoLikeAPIController {
+    public static final String ACCESS_TOKEN_PARAM_KEY = "accessToken";
+    public static final String USER_ID_PARAM_KEY = "userId";
+    public static final String CART_ITEM_ID_PARAM_KEY = "cartItemId";
+
+    private final YouMightAlsoLikeAPIService apiService;
+
+    public YouMightAlsoLikeAPIController(YouMightAlsoLikeAPIService apiService) {
+        this.apiService = apiService;
+    }
+
+    public String status(Request request, Response response) {
+        return "ok";
+    }
+
+    public String saveToDao(Request request, Response response) {
+        return "save to dao";
+    }
+
+    public String selectByCartItemsFromDao(Request request, Response response) {
+        return "select by";
+    }
 }
