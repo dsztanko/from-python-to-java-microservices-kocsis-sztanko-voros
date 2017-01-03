b9ef89c118e72d9f363a5eb410f64e33628c7949
dsztanko <szt.dori@gmail.com>
2017-01-03 11:23:02 +0100

    basic skeleton added'

--- src/main/java/you_might_also_like_service/YouMightAlsoLikeServer.java
+++ src/main/java/you_might_also_like_service/YouMightAlsoLikeServer.java
@@ -1,4 +1,31 @@
 package you_might_also_like_service;
 
+import you_might_also_like_service.controller.YouMightAlsoLikeAPIController;
+
+import java.net.URISyntaxException;
+
+import static spark.Spark.*;
+
 public class YouMightAlsoLikeServer {
+//    private YouMightAlsoLikeAPIController controller;
+
+    public static void main(String[] args) {
+
+        port(60002);
+
+        // --- MAPPINGS ---
+//        get("/api/preferences/save", application.controller::getAllEmail);
+//        get("/api/preferences/select", application.controller::changeStatus);
+
+        // --- EXCEPTION HANDLING ---
+        exception(URISyntaxException.class, (exception, request, response) -> {
+            response.status(500);
+            response.body(String.format("URI building error, maybe wrong format? : %s", exception.getMessage()));
+        });
+
+        exception(Exception.class, (exception, request, response) -> {
+            response.status(500);
+            response.body(String.format("Unexpected error occurred: %s", exception.getMessage()));
+        });
+    }
 }
