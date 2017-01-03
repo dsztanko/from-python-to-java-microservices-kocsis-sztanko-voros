16b2b5673408b223ea3dcd7be93dd6b16c67c863
vbenedek <xbenedek01@gmail.com>
2017-01-03 12:05:22 +0100

    Saving to memory and getting back filtered data from memory

--- src/main/java/you_might_also_like_service/dao/inmemoryimplementation/UserDaoMem.java
+++ src/main/java/you_might_also_like_service/dao/inmemoryimplementation/UserDaoMem.java
@@ -1,12 +1,38 @@
 package you_might_also_like_service.dao.inmemoryimplementation;
+import you_might_also_like_service.dao.UserDao;
+import you_might_also_like_service.model.User;
 
-public class UserDaoMem2 {
-    private static UserDaoMem2 ourInstance = new UserDaoMem2();
+import java.util.ArrayList;
 
-    public static UserDaoMem2 getInstance() {
+public class UserDaoMem implements UserDao{
+    private static UserDaoMem ourInstance = new UserDaoMem();
+
+    public static UserDaoMem getInstance() {
         return ourInstance;
     }
 
-    private UserDaoMem2() {
+    private ArrayList<User> DATA;
+
+    private UserDaoMem() {
+        DATA = new ArrayList<User>();
+    }
+
+    @Override
+    public void save(User user) {
+        DATA.add(user);
+    }
+
+    @Override
+    public ArrayList selectByCartItems(ArrayList<String> items) {
+        ArrayList users = new ArrayList();
+        for (User user : DATA) {
+            for (String item: items) {
+                if (user.getItems().contains(item)) {
+                    users.add(user);
+                    break;
+                }
+            }
+        }
+        return users;
     }
 }
