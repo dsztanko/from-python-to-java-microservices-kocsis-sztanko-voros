901e0d52883f4406094bc037be5e8da519c6b72f
vbenedek <xbenedek01@gmail.com>
2017-01-04 10:23:21 +0100

    recommendations done with temporary example

--- src/main/java/you_might_also_like_service/service/YouMightAlsoLikeAPIService.java
+++ src/main/java/you_might_also_like_service/service/YouMightAlsoLikeAPIService.java
@@ -27,10 +27,9 @@ public class YouMightAlsoLikeAPIService {
     }
 
     public JSONObject getRecommendations(String accessToken, String userId) {
-//        todo: in progress
         ArrayList<User> allUsers = userDao.selectByCartItems(accessToken, userId);
         User specUser = userDao.find(accessToken, userId);
-        HashMap<String, Integer> uniqueItems = userDao.selectUniqueItems(specUser.getAccessToken());
+        HashMap<String, Integer> uniqueItems = userDao.selectUniqueItems(specUser.getAccessToken(), specUser);
         for (User user : allUsers) {
             Integer power = -1;
             for (String item : user.getItems()) {
@@ -39,9 +38,38 @@ public class YouMightAlsoLikeAPIService {
                 }
             }
             for (String item : user.getItems()) {
-                uniqueItems.put(item, (int) (uniqueItems.get(item) + Math.pow(BASE_INDEX, power)));
+                if (!specUser.getItems().contains(item)) {
+                    uniqueItems.put(item, (int) (uniqueItems.get(item) + Math.pow(BASE_INDEX, power)));
+                }
             }
         }
-        return null;
+        JSONObject json = new JSONObject(uniqueItems);
+        return json;
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> al;
+        UserDaoMem a = UserDaoMem.getInstance();
+        a.save("page1", "user1", "1");
+
+        a.save("page2", "user1", "2");
+
+        a.save("page1", "user1", "3");
+
+        a.save("page1", "user2", "1");
+        a.save("page1", "user2", "4");
+        a.save("page1", "user2", "5");
+
+        a.save("page1", "user3", "1");
+        a.save("page1", "user3", "2");
+        a.save("page1", "user3", "8");
+        a.save("page1", "user3", "5");
+
+        a.save("page1", "user4", "a");
+        a.save("page1", "user4", "b");
+        a.save("page1", "user4", "c");
+        YouMightAlsoLikeAPIService b = new YouMightAlsoLikeAPIService();
+        System.out.println(b.getRecommendations("page1", "user1"));
+//        System.out.println(a.selectByCartItems("page1", "user1"));
     }
 }
