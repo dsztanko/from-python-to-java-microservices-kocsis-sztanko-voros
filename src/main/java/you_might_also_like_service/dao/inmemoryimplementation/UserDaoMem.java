532a10c54b2b3b3c730f4965f365197e5c982495
dsztanko <szt.dori@gmail.com>
2017-01-06 07:52:58 +0100

    proper logging added

--- src/main/java/you_might_also_like_service/dao/inmemoryimplementation/UserDaoMem.java
+++ src/main/java/you_might_also_like_service/dao/inmemoryimplementation/UserDaoMem.java
@@ -1,4 +1,7 @@
 package you_might_also_like_service.dao.inmemoryimplementation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import you_might_also_like_service.dao.UserDao;
 import you_might_also_like_service.model.User;
 
@@ -9,7 +12,9 @@ import java.util.HashSet;
 
 /* Implementation which stores in computer's memory */
 
-public class UserDaoMem implements UserDao{
+public class UserDaoMem implements UserDao {
+    private static final Logger logger = LoggerFactory.getLogger(UserDaoMem.class);
+
     private static UserDaoMem ourInstance = new UserDaoMem();
 
     public static UserDaoMem getInstance() {
@@ -29,24 +34,31 @@ public class UserDaoMem implements UserDao{
     // finds specUser - person, whose cart is the benchmark as opposed to the others' - in storage
     @Override
     public User find(String accessToken, String userId) {
+        logger.info(">>>>> Looking for a certain User in the storage.");
+        logger.debug(">>>>> Credentials: Access Token: {}, User ID: {}", accessToken, userId);
+
         User user = null;
-        for (User element: DATA) {
+        for (User element : DATA) {
             if (element.getAccessToken().equals(accessToken) && element.getUserID().equals(userId)) {
                 user = element;
+                logger.info(">>>>> Following user found: {}", userId);
             }
         }
         return user;
     }
 
-    // saves user
+    // saves user into storage
     @Override
     public void save(String accessToken, String userID, String item) {
+        logger.info(">>>>> Saving process just started.");
+
         User user = find(accessToken, userID);
         if (user == null) {
+            logger.info(">>>>> New User added to storage.");
             user = new User(accessToken, userID, new HashSet<>(Arrays.asList(item)));
             DATA.add(user);
-        }
-        else {
+        } else {
+            logger.info(">>>>> User already exists in storage. Updating cart with new item is done.");
             user.getItems().add(item);
         }
     }
@@ -54,6 +66,8 @@ public class UserDaoMem implements UserDao{
     // selects all the users whose cart items contains one of the specUser's cartItem
     @Override
     public ArrayList<User> containsOneOfTheSpecItemsAtLeast(String accessToken, String userId) {
+        logger.info(">>>>> Collecting all users, whose cart contains one of the specUser's items just started.");
+
         ArrayList users = new ArrayList();
         try {
             HashSet<String> items = find(accessToken, userId).getItems();
@@ -66,26 +80,31 @@ public class UserDaoMem implements UserDao{
                 }
             }
         } catch (NullPointerException e) {
-//            No access token and user ID matched.
+            logger.warn("No access token and user ID matched during searching in storage.");
         }
+        logger.debug("All users selected: {}", users);
         return users;
     }
 
     // selects all items that are unique among users selected by containsOneOfTheSpecItemsAtLeast() method
     @Override
     public HashMap<String, Integer> selectUniqueItems(String accessToken, User specUser) {
-        HashSet <String> uniqueItemsSet= new HashSet<>();
-        for (User user: containsOneOfTheSpecItemsAtLeast(accessToken, specUser.getUserID())) {
+        logger.info(">>>>> Selecting unique items just started.");
+
+        HashSet<String> uniqueItemsSet = new HashSet<>();
+        for (User user : containsOneOfTheSpecItemsAtLeast(accessToken, specUser.getUserID())) {
             if (user.getAccessToken().equals(accessToken)) {
                 uniqueItemsSet.addAll(user.getItems());
             }
         }
-        HashMap <String, Integer> uniqueItemsMap = new HashMap<>();
-        for (String id: uniqueItemsSet) {
+
+        HashMap<String, Integer> uniqueItemsMap = new HashMap<>();
+        for (String id : uniqueItemsSet) {
             if (!specUser.getItems().contains(id)) {
                 uniqueItemsMap.put(id, 0);
             }
         }
+        logger.debug(">>>>> Unique items and their importance: {}", uniqueItemsMap);
         return uniqueItemsMap;
     }
 }
