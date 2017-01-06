ca41c4da19f23ed08c71842e0dbdbb9fcc2cdb97
dsztanko <szt.dori@gmail.com>
2017-01-06 07:27:45 +0100

    proper logging added

--- src/main/java/you_might_also_like_service/controller/YouMightAlsoLikeAPIController.java
+++ src/main/java/you_might_also_like_service/controller/YouMightAlsoLikeAPIController.java
@@ -1,5 +1,7 @@
 package you_might_also_like_service.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 import you_might_also_like_service.service.YouMightAlsoLikeAPIService;
@@ -7,6 +9,8 @@ import you_might_also_like_service.service.YouMightAlsoLikeAPIService;
 /* This class is responsible for handling data extracted from the URL. */
 
 public class YouMightAlsoLikeAPIController {
+    private static final Logger logger = LoggerFactory.getLogger(YouMightAlsoLikeAPIController.class);
+
     public static final String ACCESS_TOKEN_PARAM_KEY = "accessToken";
     public static final String USER_ID_PARAM_KEY = "userId";
     public static final String CART_ITEM_ID_PARAM_KEY = "cartItemId";
@@ -18,6 +22,7 @@ public class YouMightAlsoLikeAPIController {
     }
 
     public String status(Request request, Response response) {
+        logger.debug(">>>>> Status checked.");
         response.status(200);
         return "Server runs.";
     }
@@ -27,10 +32,12 @@ public class YouMightAlsoLikeAPIController {
         String userId = request.queryParams(USER_ID_PARAM_KEY);
         String cartItemId = request.queryParams(CART_ITEM_ID_PARAM_KEY);
         if (accessToken.isEmpty() || userId.isEmpty() || cartItemId.isEmpty()) {
+            logger.error(">>>>> One of the parameters extracted from URL (/save) is null during.");
             response.status(400);
-            return "Missing parameter in URL, HTTP status error: " + response.status();
+            return ">>>>> Missing parameter in URL, HTTP status error: " + response.status();
         }
         apiService.saveUser(accessToken, userId, cartItemId);
+        logger.debug(">>>>> Values extracted from URL accepted.");
         response.status(200);
         return "Item successfully saved";
     }
@@ -39,10 +46,12 @@ public class YouMightAlsoLikeAPIController {
         String accessToken = request.queryParams(ACCESS_TOKEN_PARAM_KEY);
         String userId = request.queryParams(USER_ID_PARAM_KEY);
         if (accessToken.isEmpty() || userId.isEmpty()) {
+            logger.error(">>>>> One of the parameters extracted from URL (/select) is null.");
             response.status(400);
             return "Missing parameter in URL, HTTP status error: " + response.status();
         }
         response.status(200);
+        logger.debug(">>>>> Values extracted from URL accepted.");
         return apiService.getRecommendations(accessToken, userId).toString();
     }
 }
