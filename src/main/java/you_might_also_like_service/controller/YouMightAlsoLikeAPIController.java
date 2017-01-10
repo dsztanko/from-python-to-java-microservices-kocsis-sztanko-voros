354bc1d51e63637a19803b4d5a4178bf0d4e8f6f
dsztanko <szt.dori@gmail.com>
2017-01-10 14:36:48 +0100

    controller documented

--- src/main/java/you_might_also_like_service/controller/YouMightAlsoLikeAPIController.java
+++ src/main/java/you_might_also_like_service/controller/YouMightAlsoLikeAPIController.java
@@ -6,8 +6,15 @@ import spark.Request;
 import spark.Response;
 import you_might_also_like_service.service.YouMightAlsoLikeAPIService;
 
-/* This class is responsible for handling data extracted from the URL. */
-
+/**
+ * This class is responsible for handling data extracted from the URL.
+ *
+ * @author dsztanko
+ * @version 1.0
+ * @since 2017-01-10
+ * @see you_might_also_like_service.YouMightAlsoLikeServer
+ * @see YouMightAlsoLikeAPIService
+ */
 public class YouMightAlsoLikeAPIController {
     private static final Logger logger = LoggerFactory.getLogger(YouMightAlsoLikeAPIController.class);
 
@@ -21,12 +28,31 @@ public class YouMightAlsoLikeAPIController {
         this.apiService = apiService;
     }
 
+    /**
+     * Checks if server runs.
+     *
+     * @param request
+     * @param response
+     * @return
+     */
     public String status(Request request, Response response) {
         logger.debug(">>>>> Status checked.");
         response.status(200);
         return "Server runs.";
     }
 
+    /**
+     * Extracts values from URL and validate them.
+     * Hands them over to the API service for further
+     * process.
+     *
+     * Triggered by: /save --- end-point
+     *
+     * @param request
+     * @param response
+     * @return status 200, item saved
+     * @throws NullPointerException, if one of the parameter keys is null
+     */
     public String saveToDao(Request request, Response response) throws NullPointerException{
         String accessToken = request.queryParams(ACCESS_TOKEN_PARAM_KEY);
         String userId = request.queryParams(USER_ID_PARAM_KEY);
@@ -42,6 +68,18 @@ public class YouMightAlsoLikeAPIController {
         return "Item successfully saved";
     }
 
+    /**
+     * Extracts values from URL and validate them.
+     * Hands them over to the API service for further
+     * process.
+     *
+     * Triggered by: /select --- end-point
+     *
+     * @param request
+     * @param response
+     * @return status 200, item found, recommendation sent as JSON
+     * @throws NullPointerException, if one of the parameter keys is null
+     */
     public String selectByCartItemsFromDao(Request request, Response response) {
         String accessToken = request.queryParams(ACCESS_TOKEN_PARAM_KEY);
         String userId = request.queryParams(USER_ID_PARAM_KEY);
